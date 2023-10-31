39e32fbee3c2b0487d45edbf9936163d13a212f5
Btw-Amir <akhunamir@gmail.com>
2023-10-31 11:31:40 -0700

    new

--- src/sign-up.js
+++ src/sign-up.js
@@ -2,19 +2,20 @@ function signup(){
     var allInputs = document.getElementsByTagName('input')
     var userName = allInputs[0]
     var email = allInputs[1]
-    var pasword = allInputs[2]
+    var password = allInputs[2]
     var confirmPassword = allInputs[3]
     if(userName.value =="" ||email.value =="" ||password.value== "" || confirmPassword.value == ""){
         alert('Please fill all the inputs')
-        return
     }
-    if (pasword.value.length < 8 ){
-        alert('enter the password of 8 characters')
-        return
+    else{
+        if (password.value.length < 8 ){
+            alert('enter the password of 8 characters')
+        }else if (password.value != confirmPassword.value){
+            alert('Password is not matched ')
+        }else{
+            
+        }
     }
-    if (pasword.value != confirmPassword.value){
-        alert('Password is not matched ')
-        return
-    }
+   
     
 }
